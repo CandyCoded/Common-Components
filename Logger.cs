8795b63761fac6af56dcfeeb629c4bc159321067
Scott Doxey <neogeek0101@gmail.com>
2018-10-05 01:22:02 -0400

    Added Logger and RigidbodyMonoBehaviour components.

---
+++ Logger.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections;
+using UnityEngine;
+
+namespace CandyCoded
+{
+
+    public class Logger : MonoBehaviour
+    {
+
+        [SerializeField]
+        private string url;
+
+        private int failedConnections;
+
+        private readonly int maxFailedConnections = 10;
+
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+
+        private void OnEnable()
+        {
+
+            Application.logMessageReceived += HandleLog;
+
+        }
+
+        private void OnDisable()
+        {
+
+            Application.logMessageReceived -= HandleLog;
+
+        }
+
+#endif
+
+        private void HandleLog(string logString, string stackTrace, LogType type)
+        {
+
+            if (url != null && failedConnections < maxFailedConnections)
+            {
+
+                var loggingForm = new WWWForm();
+
+                loggingForm.AddField("Type", type.ToString());
+                loggingForm.AddField("Message", logString);
+                loggingForm.AddField("Stack_Trace", stackTrace);
+                loggingForm.AddField("Device_Model", SystemInfo.deviceModel);
+
+                StartCoroutine(SendDataToLumberLog(loggingForm));
+
+            }
+
+        }
+
+        private IEnumerator SendDataToLumberLog(WWWForm form)
+        {
+            using (WWW www = new WWW(url, form))
+            {
+
+                yield return www;
+
+                if (!string.IsNullOrEmpty(www.error))
+                {
+
+                    Debug.LogError(www.error);
+
+                    failedConnections += 1;
+
+                }
+
+            }
+
+        }
+
+    }
+
+}

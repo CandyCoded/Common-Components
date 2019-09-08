e95623f35e1a47c62e949ebda3819f8dd8b0fdcb
neogeek <neogeek0101@gmail.com>
2019-09-07 22:05:29 -0400

    Switched to using new DragMonoBehaviour.

--- Assets/Plugins/Common-Components/Scripts/DragToRotate.cs
+++ Assets/Plugins/Common-Components/Scripts/DragToRotate.cs
@@ -1,17 +1,13 @@
 // Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Collections;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace CandyCoded.CommonComponents
 {
 
-    [AddComponentMenu("CandyCoded/Drag To Rotate")]
-    public class DragToRotate : MonoBehaviour
+    public class DragToRotate : DragMonoBehaviour
     {
-
-        [SerializeField]
-        private Camera mainCamera;
 
         [SerializeField]
         private float _rotateSpeed = 100f;
@@ -19,99 +15,72 @@ namespace CandyCoded.CommonComponents
         [SerializeField]
         private float _rotationGravity = 5f;
 
+        [SerializeField]
+        private float _movementGravity = 5f;
+
 #pragma warning disable CS0649
         [SerializeField]
         private RotationAxis _rotationAxis;
 
         [SerializeField]
-        private UnityEvent RotationStarted;
-
-        [SerializeField]
-        private UnityEvent RotationEnded;
+        private SnapToRotation _snapToRotation;
 #pragma warning restore CS0649
 
-        private Transform mainCameraTransform;
+        private Coroutine _coroutine;
 
-        private int currentFingerId;
-
-        private Vector3? inputPreviousPosition;
-
-        private Vector3? delta;
-
-        private void Awake()
+        protected override void OnGrab()
         {
 
-            if (mainCamera == null)
+            if (_coroutine == null)
             {
-
-                mainCamera = Camera.main;
-
+                return;
             }
 
-            mainCameraTransform = mainCamera.transform;
+            Animate.StopAll(gameObject);
+
+            StopCoroutine(_coroutine);
+
+            _coroutine = null;
 
         }
 
-        private void Update()
+        protected override void OnDrag()
         {
 
-            if (gameObject.GetInputDown(mainCamera, out currentFingerId, out RaycastHit hit))
+            gameObject.transform.RotateWithInputDelta(_delta,
+                _rotateSpeed,
+                _mainCamera.transform, _rotationAxis);
+
+        }
+
+        protected override void OnRelease()
+        {
+
+            _coroutine = StartCoroutine(ContinueRotatingWithGravity());
+
+        }
+
+        private IEnumerator ContinueRotatingWithGravity()
+        {
+
+            while (!(_delta.magnitude < 0.01f) && !(_velocity.magnitude < 0.01f))
             {
 
-                inputPreviousPosition = InputManager.GetInputPosition(currentFingerId);
+                _delta = Vector3.Lerp(_delta, Vector3.zero, _rotationGravity * Time.deltaTime);
 
-                Animate.StopAll(gameObject);
+                _velocity = Vector3.Lerp(_velocity, Vector3.zero, _movementGravity * Time.deltaTime);
 
-                RotationStarted?.Invoke();
-
-            }
-            else if (InputManager.GetInputUp(currentFingerId))
-            {
-
-                inputPreviousPosition = null;
-
-            }
-            else if (inputPreviousPosition.HasValue)
-            {
-
-                var currentInputPosition = InputManager.GetInputPosition(currentFingerId);
-
-                if (!currentInputPosition.HasValue)
-                {
-
-                    return;
-
-                }
-
-                delta = mainCamera.ScreenToHighPrecisionViewportPoint(inputPreviousPosition.Value) -
-                        mainCamera.ScreenToHighPrecisionViewportPoint(currentInputPosition.Value);
-
-                gameObject.transform.RotateWithInputDelta(delta.Value,
+                gameObject.transform.RotateWithInputDelta(_delta,
                     _rotateSpeed,
-                    mainCameraTransform, _rotationAxis);
+                    _mainCamera.transform, _rotationAxis);
 
-                inputPreviousPosition = currentInputPosition;
+                gameObject.transform.position += _velocity;
 
-            }
-            else if (delta.HasValue)
-            {
-
-                delta = Vector3.Lerp(delta.Value, Vector3.zero, _rotationGravity * Time.deltaTime);
-
-                gameObject.transform.RotateWithInputDelta(delta.Value,
-                    _rotateSpeed,
-                    mainCameraTransform, _rotationAxis);
-
-                if (!(delta.Value.magnitude < 0.01f))
-                {
-                    return;
-                }
-
-                delta = null;
-
-                RotationEnded?.Invoke();
+                yield return null;
 
             }
+
+            _snapToRotation.SnapToRotationHandler();
 
         }
 

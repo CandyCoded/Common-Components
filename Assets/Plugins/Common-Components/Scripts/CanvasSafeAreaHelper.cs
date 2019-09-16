8a349ea6b41acf6ca49a279d9d8417ed12f87aa9
neogeek <neogeek0101@gmail.com>
2019-09-15 22:57:28 -0400

    Added CanvasSafeAreaHelper.

---
+++ Assets/Plugins/Common-Components/Scripts/CanvasSafeAreaHelper.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+namespace CandyCoded.CommonComponents
+{
+
+    [RequireComponent(typeof(Canvas))]
+    [AddComponentMenu("CandyCoded/Common Components/Canvas SafeArea Helper")]
+    public class CanvasSafeAreaHelper : MonoBehaviour
+    {
+
+        private Canvas _canvas;
+
+        private RectTransform _wrapperTransform;
+
+        private void SetSafeArea()
+        {
+
+            if (_wrapperTransform == null)
+            {
+
+                _canvas = gameObject.GetComponent<Canvas>();
+
+                var wrapper = new GameObject("SafeArea");
+
+                _wrapperTransform = wrapper.AddComponent<RectTransform>();
+
+                for (var i = 0; i < _canvas.transform.childCount; i++)
+                {
+
+                    var child = transform.GetChild(i);
+
+                    child.SetParent(_wrapperTransform, false);
+
+                }
+
+                _wrapperTransform.SetParent(_canvas.transform, false);
+
+                _wrapperTransform.offsetMin = Vector2.zero;
+                _wrapperTransform.offsetMax = Vector2.zero;
+
+            }
+
+            var safeArea = Screen.safeArea;
+
+            _wrapperTransform.anchorMin = safeArea.position / _canvas.pixelRect.size;
+            _wrapperTransform.anchorMax = (safeArea.position + safeArea.size) / _canvas.pixelRect.size;
+
+        }
+
+        private void OnRectTransformDimensionsChange()
+        {
+
+            SetSafeArea();
+
+        }
+
+    }
+
+}
